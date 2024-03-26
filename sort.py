a69cd28a067fe00258de3b39dea441e13b744f63
Brett <brett.whynot@gmail.com>
2024-03-26 05:03:52 +0000

    Add icons to sorter

--- sort.py
+++ sort.py
@@ -5,6 +5,7 @@ import json
 # Load strings.json
 strings = json.load(open("custom_components/teslemetry/strings.json"))
 en = json.load(open("custom_components/teslemetry/translations/en.json"))
+icons = json.load(open("custom_components/teslemetry/icons.json"))
 
 # Sort keys
 open("custom_components/teslemetry/strings.json", "w").write(
@@ -12,4 +13,7 @@ open("custom_components/teslemetry/strings.json", "w").write(
 )
 open("custom_components/teslemetry/translations/en.json", "w").write(
     json.dumps(en, indent=2, sort_keys=True)
+)
+open("custom_components/teslemetry/icons.json", "w").write(
+    json.dumps(icons, indent=2, sort_keys=True)
 )
