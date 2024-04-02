8f075217d735acca142ff6f59a75eb54b102eae4
Brett <brett.whynot@gmail.com>
2024-04-02 11:23:11 +0000

    Update reauth

--- custom_components/teslemetry/config_flow.py
+++ custom_components/teslemetry/config_flow.py
@@ -1,4 +1,5 @@
 """Config Flow for Teslemetry integration."""
+
 from __future__ import annotations
 
 from collections.abc import Mapping
@@ -6,12 +7,15 @@ from typing import Any
 
 from aiohttp import ClientConnectionError
 from tesla_fleet_api import Teslemetry
-from tesla_fleet_api.exceptions import InvalidToken, SubscriptionRequired, TeslaFleetError
+from tesla_fleet_api.exceptions import (
+    InvalidToken,
+    SubscriptionRequired,
+    TeslaFleetError,
+)
 import voluptuous as vol
 
+from homeassistant.config_entries import ConfigEntry, ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_ACCESS_TOKEN
-from homeassistant.config_entries import ConfigFlow
-from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN, LOGGER
@@ -23,37 +27,40 @@ DESCRIPTION_PLACEHOLDERS = {
 }
 
 
-
 class TeslemetryConfigFlow(ConfigFlow, domain=DOMAIN):
     """Config Teslemetry API connection."""
 
     VERSION = 1
+    _entry: ConfigEntry | None = None
 
-    async def async_auth(self, user_input: dict[str, str]) -> dict[str, str] | None:
+    async def async_auth(self, user_input: Mapping[str, Any]) -> dict[str, str]:
         """Reusable Auth Helper."""
         teslemetry = Teslemetry(
             session=async_get_clientsession(self.hass),
             access_token=user_input[CONF_ACCESS_TOKEN],
         )
         try:
-            await teslemetry.test()
+            metadata = await teslemetry.metadata()
         except InvalidToken:
             return {CONF_ACCESS_TOKEN: "invalid_access_token"}
         except SubscriptionRequired:
-            return {"base":"subscription_required"}
+            return {"base": "subscription_required"}
         except ClientConnectionError:
-            return {"base":"cannot_connect"}
+            return {"base": "cannot_connect"}
         except TeslaFleetError as e:
             LOGGER.error(str(e))
-            return {"base":"unknown"}
-        return None
+            return {"base": "unknown"}
+        self._entry = await self.async_set_unique_id(metadata.get("uid"))
+
+        return {}
 
     async def async_step_user(
         self, user_input: Mapping[str, Any] | None = None
-    ) -> FlowResult:
+    ) -> ConfigFlowResult:
         """Get configuration from the user."""
         errors: dict[str, str] = {}
         if user_input and not (errors := await self.async_auth(user_input)):
+            self._abort_if_unique_id_configured()
             return self.async_create_entry(
                 title="Teslemetry",
                 data=user_input,
@@ -68,32 +75,28 @@ class TeslemetryConfigFlow(ConfigFlow, domain=DOMAIN):
 
     async def async_step_reauth(
         self, entry_data: Mapping[str, Any]
-    ) -> FlowResult:
+    ) -> ConfigFlowResult:
         """Handle reauth on failure."""
-        self._reauth_entry = self.hass.config_entries.async_get_entry(
-            self.context["entry_id"]
-        )
-        print(entry_data)
         return await self.async_step_reauth_confirm(entry_data)
 
     async def async_step_reauth_confirm(
-        self, user_input: dict[str, str] | None = None
-    ) -> FlowResult:
+        self, user_input: Mapping[str, Any] | None = None
+    ) -> ConfigFlowResult:
         """Handle users reauth credentials."""
 
         errors: dict[str, str] | None = None
 
         if user_input and not (errors := await self.async_auth(user_input)):
-            if self._reauth_entry:
+            if self._entry:
                 self.hass.config_entries.async_update_entry(
-                    self._reauth_entry,
+                    self._entry,
                     data=user_input,
                 )
                 self.hass.async_create_task(
-                    self.hass.config_entries.async_reload(self._reauth_entry.entry_id)
+                    self.hass.config_entries.async_reload(self._entry.entry_id)
                 )
                 return self.async_abort(reason="reauth_successful")
-            return self.async_create_entry(title=self._reauth_username, data=user_input)
+            return self.async_create_entry(title="Teslemetry", data=user_input)
 
         return self.async_show_form(
             step_id="reauth_confirm",
