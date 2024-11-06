b444cfe3b50d4ab5f81e62203df7ee66689a35a4
Brett Adams <brett.whynot@gmail.com>
2024-11-06 07:25:18 +0000

    Fix route tracker

--- custom_components/teslemetry/device_tracker.py
+++ custom_components/teslemetry/device_tracker.py
@@ -112,7 +112,7 @@ class TeslemetryDeviceTrackerRouteEntity(TeslemetryDeviceTrackerEntity):
     @property
     def location_name(self) -> str | None:
         """Return a location name for the current location of the device."""
-        location = self.get("drive_state_active_route_name")
-        if location == "Home":
+        location = self.get("drive_state_active_route_destination")
+        if location === "Home":
             return STATE_HOME
         return location
