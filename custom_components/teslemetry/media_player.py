df6b59d1aaf2c7c41874a4f97d8757d3eaae6178
Brett <brett.whynot@gmail.com>
2025-02-22 15:44:41 +1000

    Update media_player.py

--- custom_components/teslemetry/media_player.py
+++ custom_components/teslemetry/media_player.py
@@ -1,5 +1,6 @@
 """Media Player platform for Teslemetry integration."""
 from __future__ import annotations
+from homeassistant.helpers.restore_state import RestoreEntity
 from tesla_fleet_api.const import Scope
 
 from homeassistant.components.media_player import (
@@ -11,10 +12,14 @@ from homeassistant.components.media_player import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from tesla_fleet_api.vehiclesigned import VehicleSpecific
+
+from custom_components.teslemetry.helpers import handle_vehicle_command
 
 from .entity import (
+    TeslemetryRootEntity,
     TeslemetryVehicleEntity,
-    TeslemetryVehicleComplexStreamEntity
+    TeslemetryVehicleStreamEntity
 )
 from .models import TeslemetryVehicleData
 
@@ -24,7 +29,17 @@ STATES = {
     "Paused": MediaPlayerState.PAUSED,
     "Stopped": MediaPlayerState.IDLE,
 }
-MAX_VOLUME = 11.0
+DISPLAY_STATES = {
+    "On": MediaPlayerState.IDLE,
+    "Accessory": MediaPlayerState.IDLE,
+    "Charging": MediaPlayerState.OFF,
+    "Sentry": MediaPlayerState.OFF,
+    "Off": MediaPlayerState.OFF,
+}
+# Tesla uses 31 steps, in 0.333 increments up to 10.333
+VOLUME_STEP = 1 / 31
+VOLUME_FACTOR = 31 / 3 # 10.333
+
 
 
 async def async_setup_entry(
@@ -41,24 +56,19 @@ async def async_setup_entry(
     )
 
 
-class TeslemetryMediaEntity(TeslemetryVehicleEntity, MediaPlayerEntity):
+class TeslemetryMediaEntity(TeslemetryRootEntity, MediaPlayerEntity):
     """Base vehicle media player class."""
 
+    api: VehicleSpecific
+
     _attr_device_class = MediaPlayerDeviceClass.SPEAKER
-    _attr_supported_features = (
-        MediaPlayerEntityFeature.NEXT_TRACK
-        | MediaPlayerEntityFeature.PAUSE
-        | MediaPlayerEntityFeature.PLAY
-        | MediaPlayerEntityFeature.PREVIOUS_TRACK
-        | MediaPlayerEntityFeature.VOLUME_SET
-    )
-    max_volume: float = MAX_VOLUME
+    _attr_volume_step = VOLUME_STEP
 
     async def async_set_volume_level(self, volume: float) -> None:
         """Set volume level, range 0..1."""
         self.raise_for_scope(Scope.VEHICLE_CMDS)
 
-        await self.handle_command(self.api.adjust_volume(int(volume * self.max_volume)))
+        await handle_vehicle_command(self.api.adjust_volume(volume*VOLUME_FACTOR))
         self._attr_volume_level = volume
         self.async_write_ha_state()
 
@@ -67,32 +77,33 @@ class TeslemetryMediaEntity(TeslemetryVehicleEntity, MediaPlayerEntity):
         if self.state != MediaPlayerState.PLAYING:
             self.raise_for_scope(Scope.VEHICLE_CMDS)
 
-            await self.handle_command(self.api.media_toggle_playback())
+            await handle_vehicle_command(self.api.media_toggle_playback())
             self._attr_state = MediaPlayerState.PLAYING
             self.async_write_ha_state()
 
     async def async_media_pause(self) -> None:
         """Send pause command."""
+
         if self.state == MediaPlayerState.PLAYING:
             self.raise_for_scope(Scope.VEHICLE_CMDS)
 
-            await self.handle_command(self.api.media_toggle_playback())
+            await handle_vehicle_command(self.api.media_toggle_playback())
             self._attr_state = MediaPlayerState.PAUSED
             self.async_write_ha_state()
 
     async def async_media_next_track(self) -> None:
         """Send next track command."""
-        self.raise_for_scope(Scope.VEHICLE_CMDS)
 
-        await self.handle_command(self.api.media_next_track())
+        self.raise_for_scope(Scope.VEHICLE_CMDS)
+        await handle_vehicle_command(self.api.media_next_track())
 
     async def async_media_previous_track(self) -> None:
         """Send previous track command."""
+
         self.raise_for_scope(Scope.VEHICLE_CMDS)
+        await handle_vehicle_command(self.api.media_prev_track())
 
-        await self.handle_command(self.api.media_prev_track())
-
-class TeslemetryPollingMediaEntity(TeslemetryVehicleEntity, MediaPlayerEntity):
+class TeslemetryPollingMediaEntity(TeslemetryVehicleEntity, TeslemetryMediaEntity):
     """Polling vehicle media player class."""
 
     def __init__(
@@ -102,24 +113,24 @@ class TeslemetryPollingMediaEntity(TeslemetryVehicleEntity, MediaPlayerEntity):
     ) -> None:
         """Initialize the media player entity."""
         super().__init__(data, "media")
+
+        self._attr_supported_features = (
+            MediaPlayerEntityFeature.NEXT_TRACK
+            | MediaPlayerEntityFeature.PAUSE
+            | MediaPlayerEntityFeature.PLAY
+            | MediaPlayerEntityFeature.PREVIOUS_TRACK
+            | MediaPlayerEntityFeature.VOLUME_SET
+        )
         self.scoped = scoped
         if not scoped:
             self._attr_supported_features = MediaPlayerEntityFeature(0)
 
     def _async_update_attrs(self) -> None:
         """Update entity attributes."""
-        self.max_volume = self.get("vehicle_state_media_info_audio_volume_max", MAX_VOLUME)
-        self._attr_state = STATES.get(
-            self.get("vehicle_state_media_info_media_playback_status"),
-            MediaPlayerState.OFF,
-        )
-        self._attr_volume_step = (
-            1.0
-            / self.max_volume
-            / self.get("vehicle_state_media_info_audio_volume_increment", 1.0 / 3)
-        )
+        state = self.get("vehicle_state_media_info_media_playback_status")
+        self._attr_state = STATES[state] if state in STATES else None
         self._attr_volume_level = (
-            self.get("vehicle_state_media_info_audio_volume", 0) / self.max_volume
+            self.get("vehicle_state_media_info_audio_volume", 0) / VOLUME_FACTOR
         )
 
         if duration := self.get("vehicle_state_media_info_now_playing_duration"):
@@ -146,7 +157,7 @@ class TeslemetryPollingMediaEntity(TeslemetryVehicleEntity, MediaPlayerEntity):
         )
         self._attr_source = self.get("vehicle_state_media_info_now_playing_source")
 
-class TeslemetryStreamingMediaEntity(TeslemetryVehicleComplexStreamEntity, MediaPlayerEntity):
+class TeslemetryStreamingMediaEntity(TeslemetryVehicleStreamEntity, TeslemetryMediaEntity, RestoreEntity):
     """Streaming vehicle media player class."""
 
     def __init__(
@@ -155,12 +166,133 @@ class TeslemetryStreamingMediaEntity(TeslemetryVehicleComplexStreamEntity, Media
         scoped: bool,
     ) -> None:
         """Initialize the media player entity."""
-        super().__init__(data, "media", [])
+        super().__init__(data, "media")
+
+        self._attr_supported_features = (
+            MediaPlayerEntityFeature.NEXT_TRACK
+            | MediaPlayerEntityFeature.PAUSE
+            | MediaPlayerEntityFeature.PLAY
+            | MediaPlayerEntityFeature.PREVIOUS_TRACK
+            | MediaPlayerEntityFeature.VOLUME_SET
+        )
         self.scoped = scoped
         if not scoped:
             self._attr_supported_features = MediaPlayerEntityFeature(0)
 
-    def _async_update_attrs(self) -> None:
+
+    async def async_added_to_hass(self) -> None:
+        """Call when entity is added to hass."""
+
+        await super().async_added_to_hass()
+        if (state := await self.async_get_last_state()) is not None:
+            self._attr_state = None if state.state == "Unknown" else MediaPlayerState(state.state)
+            self._attr_volume_level = state.attributes.get("volume_level")
+            self._attr_media_title = state.attributes.get("media_title")
+            self._attr_media_artist = state.attributes.get("media_artist")
+            self._attr_media_album_name = state.attributes.get("media_album_name")
+            self._attr_media_playlist = state.attributes.get("media_playlist")
+            self._attr_source = state.attributes.get("source")
+
+            self.async_write_ha_state()
+
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_CenterDisplay(
+                self._async_handle_center_display
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaPlaybackStatus(
+                self._async_handle_media_playback_status
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaPlaybackSource(
+                self._async_handle_media_playback_source
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaAudioVolume(
+                self._async_handle_media_audio_volume
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaNowPlayingDuration(
+                self._async_handle_media_now_playing_duration
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaNowPlayingElapsed(
+                self._async_handle_media_now_playing_elapsed
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaNowPlayingArtist(
+                self._async_handle_media_now_playing_artist
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaNowPlayingAlbum(
+                self._async_handle_media_now_playing_album
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaNowPlayingTitle(
+                self._async_handle_media_now_playing_title
+            )
+        )
+        self.async_on_remove(
+            self.vehicle.stream_vehicle.listen_MediaNowPlayingStation(
+                self._async_handle_media_now_playing_station
+            )
+        )
+
+    def _async_handle_center_display(self, value: str | None) -> None:
         """Update entity attributes."""
-        pass
-        # NOT IMPLEMENTED BY TESLA YET
+        if value is not None:
+            self._attr_state = DISPLAY_STATES.get(value)
+            self.async_write_ha_state()
+
+    def _async_handle_media_playback_status(self, value: str | None) -> None:
+        """Update entity attributes."""
+        self._attr_state = MediaPlayerState.OFF if value is None else STATES.get(value)
+        self.async_write_ha_state()
+
+    def _async_handle_media_playback_source(self, value: str | None) -> None:
+        """Update entity attributes."""
+        self._attr_source = value
+        self.async_write_ha_state()
+
+    def _async_handle_media_audio_volume(self, value: float | None) -> None:
+        """Update entity attributes."""
+        self._attr_volume_level = None if value is None else value / VOLUME_FACTOR
+        self.async_write_ha_state()
+
+    def _async_handle_media_now_playing_duration(self, value: int | None) -> None:
+        """Update entity attributes."""
+        self._attr_media_duration = None if value is None else int(value / 1000)
+        self.async_write_ha_state()
+
+    def _async_handle_media_now_playing_elapsed(self, value: int | None) -> None:
+        """Update entity attributes."""
+        self._attr_media_position = None if value is None else int(value / 1000)
+        self.async_write_ha_state()
+
+    def _async_handle_media_now_playing_artist(self, value: str | None) -> None:
+        """Update entity attributes."""
+        self._attr_media_artist = value #Check if this is album artist or not
+        self.async_write_ha_state()
+
+    def _async_handle_media_now_playing_album(self, value: str | None) -> None:
+        """Update entity attributes."""
+        self._attr_media_album_name = value
+        self.async_write_ha_state()
+
+    def _async_handle_media_now_playing_title(self, value: str | None) -> None:
+        """Update entity attributes."""
+        self._attr_media_title = value
+        self.async_write_ha_state()
+
+    def _async_handle_media_now_playing_station(self, value: str | None) -> None:
+        """Update entity attributes."""
+        self._attr_media_channel = value # could also be _attr_media_playlist when Spotify
+        self.async_write_ha_state()
