2fd84d319870ce164fedbded98aedac8b0863992
Brett <brett.whynot@gmail.com>
2025-04-22 21:50:22 +1000

    Update release.sh

--- release.sh
+++ release.sh
@@ -9,8 +9,8 @@ uv pip install -r requirements_test_all.txt
 echo "Enter the target version:"
 read VERSION
 
-git branch -D v$VERSION
-git checkout -b v$VERSION
+git branch -D release-$VERSION
+git checkout -b release-$VERSION
 
 for PR_NUMBER in $(gh pr list --repo home-assistant/core --author Bre77 --state open --json number | jq -r '.[].number'); do
     PR_TITLE=$(gh pr view $PR_NUMBER --repo home-assistant/core --json title | jq -r '.title')
@@ -25,8 +25,11 @@ done
 yq -i -o json '.version="$VERSION"' "homeassistant/components/teslemetry/manifest.json"
 git commit -am "v$VERSION" --no-verify
 
-pytest tests/components/teslemetry
+pytest tests/components/teslemetry --snapshot-update
+
+read -p "Press Enter to continue..."
 
 git tag -a v$VERSION -m "Release $VERSION"
 git push origin v$VERSION
+git push
 git checkout main
