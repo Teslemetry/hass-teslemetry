677411640625db344be870e25f34c138e1bee3f6
Brett <brett.whynot@gmail.com>
2025-04-22 17:28:50 +1000

    Update release.sh

--- release.sh
+++ release.sh
@@ -3,9 +3,10 @@ TARGET="2025.4.0"
 git fetch upstream $TARGET
 git rebase upstream/$TARGET
 
-for BRANCH in $(gh pr list --repo home-assistant/core --author Bre77 --state open --json headRefName --jq '.[].headRefName'); do
-  echo "Merging branch $BRANCH from fork"
-  git fetch fork $BRANCH
-  git merge --squash fork/$BRANCH
+for PR in $(gh pr list --repo home-assistant/core --author Bre77 --state open --json number --jq '.[].number'); do
+  echo "Applying patch from PR #$PR"
+  curl -L https://github.com/home-assistant/core/pull/$PR.patch -o $PR.patch
+  git apply -3 $PR.patch
   git mergetool
+  rm *.patch
 done
