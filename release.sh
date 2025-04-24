3de393bbaf58ff6cb15049e5c844d0b1340f46d0
Brett <brett.whynot@gmail.com>
2025-04-24 13:42:37 +1000

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,8 +2,6 @@
 git fetch upstream dev
 git rebase upstream/dev
 git push --force-with-lease
-script/setup
-uv pip install -r requirements_test_all.txt
 
 # Ask for version
 echo "Enter the target version:"
@@ -11,6 +9,8 @@ read VERSION
 
 git branch -D release-$VERSION
 git checkout -b release-$VERSION
+
+rm release_notes.txt
 
 for PR_NUMBER in $(gh pr list --repo home-assistant/core --author Bre77 --state open --json number | jq -r '.[].number'); do
     PR_TITLE=$(gh pr view $PR_NUMBER --repo home-assistant/core --json title | jq -r '.title')
@@ -20,16 +20,23 @@ for PR_NUMBER in $(gh pr list --repo home-assistant/core --author Bre77 --state 
     git mergetool
     rm *.patch
     git commit -am "#$PR_NUMBER: $PR_TITLE" --no-verify
+    echo "#$PR_NUMBER: $PR_TITLE" >> release_notes.txt
 done
 
 yq -i -o json ".version=\"$VERSION\"" "homeassistant/components/teslemetry/manifest.json"
+echo "" > release_notes.txt
+echo "**Full Changelog**: https://github.com/Teslemetry/hass-teslemetry/commits/v$VERSION" > release_notes.txt
+
 git commit -am "v$VERSION" --no-verify
 
+script/setup
+uv pip install -r requirements_test_all.txt
 pytest tests/components/teslemetry
 
 read -p "Press Enter to release..."
 
 git tag -a v$VERSION -m "Release $VERSION"
 git push origin v$VERSION
+gh release create v$VERSION -p -F release_notes.txt
 git push
 git checkout main
