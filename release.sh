0cb2d82110684409cc94b1f8b8e5a861dfe9859b
Brett <brett.whynot@gmail.com>
2025-04-24 13:55:37 +1000

    Prepare custom integration

---
+++ release.sh
@@ -0,0 +1,42 @@
+# Get everything up to date
+git fetch upstream dev
+git rebase upstream/dev
+git push --force-with-lease
+
+# Ask for version
+echo "Enter the target version:"
+read VERSION
+
+git branch -D release-$VERSION
+git checkout -b release-$VERSION
+
+rm release_notes.txt
+
+for PR_NUMBER in $(gh pr list --repo home-assistant/core --author Bre77 --state open --json number | jq -r '.[].number'); do
+    PR_TITLE=$(gh pr view $PR_NUMBER --repo home-assistant/core --json title | jq -r '.title')
+    echo "Applying patch from PR #$PR_NUMBER: $PR_TITLE"
+    curl -L https://github.com/home-assistant/core/pull/$PR_NUMBER.patch -o $PR_NUMBER.patch
+    git apply -3 $PR_NUMBER.patch
+    git mergetool
+    rm *.patch
+    git commit -am "#$PR_NUMBER: $PR_TITLE" --no-verify
+    echo "#$PR_NUMBER: $PR_TITLE" >> release_notes.txt
+done
+
+yq -i -o json ".version=\"$VERSION\"" "homeassistant/components/teslemetry/manifest.json"
+echo "" >> release_notes.txt
+echo "**Full Changelog**: https://github.com/Teslemetry/hass-teslemetry/commits/v$VERSION" >> release_notes.txt
+
+git commit -am "v$VERSION" --no-verify
+
+script/setup
+uv pip install -r requirements_test_all.txt
+pytest tests/components/teslemetry
+
+read -p "Press Enter to release..."
+
+git tag -a v$VERSION -m "Release $VERSION"
+git push origin v$VERSION
+gh release create v$VERSION -p -F release_notes.txt
+git push
+git checkout main
