2357bf076358619f8e44185e47388aa26034f496
Brett Adams <brett.whynot@gmail.com>
2026-02-10 09:02:41 +1000

    #161956: Complete config-entry-unloading quality check in Teslemetry

--- homeassistant/components/teslemetry/__init__.py
+++ homeassistant/components/teslemetry/__init__.py
@@ -204,9 +204,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: TeslemetryConfigEntry) -
                     manual=True,
                 )
 
-            remove_listener = stream.async_add_listener(
-                create_handle_vehicle_stream(vin, coordinator),
-                {"vin": vin},
+            entry.async_on_unload(
+                stream.async_add_listener(
+                    create_handle_vehicle_stream(vin, coordinator),
+                    {"vin": vin},
+                )
             )
             stream_vehicle = stream.get_vehicle(vin)
             poll = vehicle_metadata[vin].get("polling", False)
@@ -222,7 +224,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: TeslemetryConfigEntry) -
                     vin=vin,
                     firmware=firmware,
                     device=device,
-                    remove_listener=remove_listener,
                 )
             )
 
@@ -331,6 +332,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: TeslemetryConfigEntry) -
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     if stream:
+        entry.async_on_unload(stream.close)
         entry.async_create_background_task(hass, stream.listen(), "Teslemetry Stream")
 
     return True
